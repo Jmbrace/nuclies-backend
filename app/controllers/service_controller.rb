3e37e98adabccd555b0803a4ca6d91ca49e962f9
Jmbrace <jmbrace@ilstu.edu>
2016-10-23 00:47:26 -0500

    pushing to prod

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -76,7 +76,7 @@ class ServiceController < ApplicationController
     if !volunteer.nil?
       Service.where(being_served: false).find_each do |service|
         if(volunteer.services.include?(service.name) || service.name == "custom")
-          service.in_need = InNeed.find(service.in_need_id])
+          service.in_need = InNeed.find(service.in_need_id)
           services_to_return.push(service.as_json)
         end
       end
